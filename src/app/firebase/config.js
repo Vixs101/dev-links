d1e0dd800a69413bc7e18b61d9c15ad1d7fe7c5e
Vixs101 <elijahvix695@gmail.com>
2024-09-09 16:44:12 +0100

    Chore:moved the api keys in a .env file

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -1,14 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCk13ajBKO67u6SRe6kvzSt_mVpWJYZzdo",
-  authDomain: "dev-links-7d63a.firebaseapp.com",
-  projectId: "dev-links-7d63a",
-  storageBucket: "dev-links-7d63a.appspot.com",
-  messagingSenderId: "629422293643",
-  appId: "1:629422293643:web:286b98d0fb22b4458a4a8e"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
